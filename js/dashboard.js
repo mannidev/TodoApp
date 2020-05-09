8706fb47910c0faa984d2d4b4dede4b2767ea0bb
kjoedev <kjoedev@gmail.com>
2020-05-08 22:18:13 -0400

    clean code

--- js/dashboard.js
+++ js/dashboard.js
@@ -10,11 +10,20 @@ window.onload = (event) => {
     }
 }
 
-function addNewGroup() {
+function showNewGroupSection() {
     const summary = document.getElementById('todo-summary');
     const newGroup = document.getElementById('new-todo-Group');
 
     summary.classList.add('hide');
     newGroup.classList.add('show');
     newGroup.classList.remove('hide');
+}
+
+function addNewGroup() {
+    const toast = document.getElementById('toast');
+    toast.classList.add('show-toast');
+    toast.innerHTML = 'New Todo Group Successfully Added';
+    setTimeout(() => {
+        toast.classList.remove('show-toast')
+    }, 3000);
 }
